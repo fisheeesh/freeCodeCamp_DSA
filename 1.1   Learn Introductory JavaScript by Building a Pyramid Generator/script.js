4f440da57cc8e4844d30501ab37474bcd03a4b88
fisheeesh <swanphyo444@gmail.com>
2025-06-29 19:26:28 +0700

    1.3 completed

--- 1.1   Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ 1.1   Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1,7 +1,7 @@
 const character = "!";
 const count = 10;
 const rows = [];
-let inverted = false;
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
