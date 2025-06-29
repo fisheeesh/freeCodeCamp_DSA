1f6a58503503bacc8ece04e07b8c332d16b70965
fisheeesh <swanphyo444@gmail.com>
2025-06-29 19:59:08 +0700

    Certification

---
+++ 1.8 🎓 Build a Palindrome Checker/script.js
@@ -0,0 +1,25 @@
+const inputEl = document.getElementById('text-input')
+const checkBtn = document.getElementById('check-btn')
+const resultEl = document.getElementById('result')
+
+function palindromeChekcer() {
+    const input = inputEl.value.trim()
+    resultEl.textContent = ''
+    if (input === '') {
+        alert('Please input a value')
+        return
+    }
+
+    const cleaned = input.replace(/[^\da-zA-Z]/, "").toLowerCase()
+    const reversed = cleaned.split('').reverse().join("")
+
+    resultEl.textContent = cleaned === reversed ? `${cleaned} is a palindrome` : `${cleaned} is not a palindrome`
+}
+
+checkBtn.addEventListener('click', palindromeChekcer)
+
+inputEl.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+        palindromeChekcer()
+    }
+})
