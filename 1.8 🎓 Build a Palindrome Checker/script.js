53255c58735b64161e742f27e721832f78c96ce1
fisheeesh <swanphyo444@gmail.com>
2025-06-30 01:37:17 +0700

    Module 1 certification

--- 1.8 🎓 Build a Palindrome Checker/script.js
+++ 1.8 🎓 Build a Palindrome Checker/script.js
@@ -3,19 +3,18 @@ const checkBtn = document.getElementById('check-btn')
 const resultEl = document.getElementById('result')
 
 function palindromeChekcer() {
-    const input = inputEl.value.trim()
+    const input = inputEl.value
     resultEl.textContent = ''
     if (input === '') {
         alert('Please input a value')
         return
     }
-
     //* Remove all non-alphanumeric characters (punctuation, spaces and symbols) 
     //* And turn everything into the same case (lower or upper case) in order to check for palindromes.
-    const cleaned = input.replace(/[^\da-zA-Z]/, "").toLowerCase()
+    const cleaned = input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
     const reversed = cleaned.split('').reverse().join("")
 
-    resultEl.textContent = cleaned === reversed ? `${cleaned} is a palindrome` : `${cleaned} is not a palindrome`
+    resultEl.textContent = cleaned === reversed ? `${inputEl.value} is a palindrome` : `${inputEl.value} is not a palindrome`
 }
 
 checkBtn.addEventListener('click', palindromeChekcer)
